b0e09f3e1ffad53b62bc6c3877b863ff65b4b0be
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-19 21:52:37 +0530

    Some message

--- common.sh
+++ common.sh
@@ -51,6 +51,16 @@ VALIDATE $? "Istall dependencies"
 
 }
 
+java_setup(){
+    dnf install maven -y &>>$LOG_FILE
+    VALIDATE $? "Installing Maven"
+    mvn clean package  &>>$LOG_FILE
+    VALIDATE $? "Packing the application"
+    mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+    VALIDATE $? "Renaming the artifact"
+
+}
+
 app_setup(){
 
     id roboshop &>>$LOG_FILE
