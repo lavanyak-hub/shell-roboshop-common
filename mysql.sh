48232f529bb3059f9d7b1fa80159dcdfe7ef4a5f
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-19 21:29:19 +0530

    Some message

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ VALIDATE $? "Installing MySQL Server"
 systemctl enable mysqld &>>$LOG_FILE
 VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
-VALIADTE $? "Starting MySQL Server"
+VALIDATE $? "Starting MySQL Server"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
 VALIDATE $? "Setting root password to MySQL Server"
 
