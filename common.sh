bf8060bedd83847b8cfe0cea7336a9a7104d092a
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-19 20:35:48 +0530

    Some message

--- common.sh
+++ common.sh
@@ -11,6 +11,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.rakesh12.fun
 
 mkdir -p $LOGS_FOLDER
 echo "script started executed at: $(date)" | tee -a $LOG_FILE
@@ -36,6 +37,48 @@ else
     fi
 }
 
+nodejs_setup(){
+     dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Istall dependencies"
+
+}
+
+app_setup(){
+    mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading $app_name application"
+
+cd /app
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/$app_name.zip &>>$LOG_FILE
+VALIDATE $? "unzip $app_name"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+VALIDATE $? "Copy systemctl service"
+systemctl daemon-reload
+systemctl enable $app_name &>>$LOG_FILE
+VALIDATE $? "Enable $app_name"
+
+}
+app_restart(){
+    systemctl restart $app_name
+VALIDATE $? "Restarted $app_name"
+}
 print_total_time(){
     END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
