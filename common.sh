5a65fc71f503fa30bdd2f02ef9d2f405c503a35a
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-19 22:03:00 +0530

    Some message

--- common.sh
+++ common.sh
@@ -61,6 +61,13 @@ java_setup(){
 
 }
 
+python_setup(){
+
+    dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+    VALIDATE $? "Installing Python3"
+    pip3 install -r requirements.txt &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+}
 app_setup(){
 
     id roboshop &>>$LOG_FILE
